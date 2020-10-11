cfc3ae36382a0c7259ef2372c9e49f7637146959
FM8299COM <karlis21kesans@gmail.com>
2020-10-11 20:04:01 +0300

    First innit

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+const api = {
+    key: "b8514b51918db70dfb9c5951ccd0fdad",
+    baseurl: "https://api.openweathermap.org/data/2.5/"
+}
+
+
+window.addEventListener('load', function(){
+    let btnTempB = document.getElementById('TempB')
+
+    btnTempB.addEventListener('click', async function(){
+        let response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=56.9496&lon=24.1052&appid=b8514b51918db70dfb9c5951ccd0fdad')
+        let data_json = await response.json()
+        console.log(data_json)
+        
+        let TempS = document.getElementById('TempS')
+        TempS.innerHTML = data_json.main.temp
+
+        let TempMin = document.getElementById('TempMin')
+        TempMin.innerHTML = data_json.main.temp_min
+
+        let TempMax = document.getElementById('TempMax')
+        TempMax.innerHTML = data_json.main.temp_max
+
+        let Humidity = document.getElementById('Humidity')
+        Humidity.innerHTML = data_json.main.humidity
+
+    })
+})
